f3b2988c3864575e60eca53228cda50f37ca59b1
zhoucb50741 <zhoucb50741@hundsun.com>
2024-02-04 23:10:14 +0800

    完善首页推文样式，完善评论区样式，博客模块后端创建

---
+++ ruoyi-modules/biz-blog/src/main/java/com/ruoyi/blog/BlogApplication.java
@@ -0,0 +1,21 @@
+package com.ruoyi.blog;
+
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import com.ruoyi.common.swagger.annotation.EnableCustomSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableRyFeignClients
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  博客服务模块启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+
+}
--- ruoyi-ui/src/views/biz/blog/index.vue
+++ ruoyi-ui/src/views/biz/blog/index.vue
@@ -1,21 +1,4 @@
 <template>
-  <!--  <el-container>
-      &lt;!&ndash; 左侧目录   &ndash;&gt;
-      <el-aside width="200px">
-
-      </el-aside>
-      &lt;!&ndash;中部推送帖子&ndash;&gt;
-      <el-main>
-
-
-      </el-main>
-
-      &lt;!&ndash; 右侧热榜 &ndash;&gt;
-      <el-container>
-
-
-      </el-container>
-    </el-container>-->
   <div style="background-color: #EEF1F6FF">
     <div class="fixed-box-left">
       <el-menu default-active="1">
@@ -52,7 +35,7 @@
 
     <div class="recommended-post">
       <el-main>
-        <el-card v-for="blog in this.testBlogs" :key="blog.username" shadow="hover" style="margin-bottom: 20px">
+        <el-card v-for="blog in this.testBlogs" :key="blog.id" shadow="hover" style="margin-bottom: 20px">
           <div slot="header" class="clearfix">
             <div style="width: 60px; float: left">
               <el-avatar :size="50" src="http://121.43.39.16:8082/ruoyi-test/2024/02/02/2b_20240202165949A006.jpg"/>
@@ -61,31 +44,158 @@
               <div>{{ blog.username }}</div>
               <div style="margin-top: 10px">{{ blog.postTime }}</div>
             </div>
-            <el-button style="float: right; padding: 10px" type="primary">关注</el-button>
+            <el-button style="float: right; padding: 10px" type="primary" @click="testMethod()">关注</el-button>
           </div>
           <div class="card-content">{{ blog.content }}</div>
           <div class="card-button">
-            <el-button icon="el-icon-search" type="primary">查看</el-button>
+            <el-badge :value="666" type="primary">
+              <el-button icon="el-icon-search" type="primary" @click="showPost(blog.id)">查看</el-button>
+            </el-badge>
           </div>
-          <div class="card-button">
-            <el-button icon="el-icon-edit" type="warning">评论</el-button>
-          </div>
-          <div class="card-button">
-            <el-button icon="el-icon-thumb" type="danger">点赞</el-button>
+          <!--          <div class="card-button">
+                      <el-badge :value="666" type="warning">
+                        <el-button icon="el-icon-edit" type="warning">评论</el-button>
+                      </el-badge>
+                    </div>-->
+          <div class="card-button" @click="testMethod()">
+            <el-badge :value="666" type="danger">
+              <el-button icon="el-icon-thumb" type="danger">点赞</el-button>
+            </el-badge>
           </div>
         </el-card>
       </el-main>
     </div>
 
 
-    <!--    <div class="fixed-box-right">
-          最新发布
-          <el-menu>
-            <el-menu-item v-for="trend in this.testTrends" :key="trend.id">
-              {{ trend.title }}
-            </el-menu-item>
-          </el-menu>
-        </div>-->
+    <el-dialog :before-close="handleClose" :close-on-click-modal="false" :visible.sync="this.showPostDetail"
+               title="推文详情">
+      <!--   标题下灰色副标题区域   -->
+      <div class="post-sub-title">
+        <div class="post-sub-title-unit">
+          <i class="el-icon-user" style="padding-left: 10px"> {{ this.showUser.username }}</i>
+        </div>
+        <div class="post-sub-title-unit"><i class="el-icon-timer"> 于 {{ this.showUser.postTime }} 发布</i></div>
+        <div class="post-sub-title-unit"><i class="el-icon-view"> 浏览量:1010</i></div>
+        <div class="post-sub-title-unit"><i class="el-icon-thumb"> 点赞:999</i></div>
+        <div class="post-sub-title-unit"><i class="el-icon-chat-line-round"> 评论数:699</i></div>
+      </div>
+
+      <!--   对话框内容部分   -->
+      <div class="comment-container">
+
+        <div style="padding-bottom: 30px">
+          {{ this.showUser.content }}
+        </div>
+
+        <!--   评论标题     -->
+        <div style="width: 100%;border-top: 1px solid #EEF1F6FF">
+          <h1>评论 {{ this.testComment.cnt }}</h1>
+        </div>
+
+        <!--   评论输入区域   -->
+        <div style="width: 70%">
+          <el-input
+            v-model="commentInput"
+            :autosize="true"
+            :rows="2"
+            :show-word-limit="true"
+            maxlength="500"
+            placeholder="请友善发言哦~~~~"
+            resize="none"
+            type="textarea"
+          />
+        </div>
+
+        <div style="width: 30%;padding-left: 20%">
+          <el-button round type="primary" @click="testMethod()">回复</el-button>
+        </div>
+
+        <div style="width: 100%">
+          <!--   母评论列表     -->
+          <div v-for="pComment in this.testComment.content"
+               style="display: flex;flex-wrap: wrap;padding-top: 20px;border-bottom: 1px solid #EEF1F6FF">
+            <!-- 左部头像框 -->
+            <div style="margin-right: 10px">
+              <el-avatar :size="35" src="http://121.43.39.16:8082/ruoyi-test/2024/02/02/2b_20240202165949A006.jpg"/>
+            </div>
+
+            <!-- 右部框,分为两部分，上部分的回复者、时间信息、点赞按钮等，下部分的回复内容 -->
+            <div style="width: 95%">
+              <div style="margin-bottom: 10px;display: flex;flex-wrap: wrap">
+                <div style="margin-right: 10px">{{ pComment.senderName }}</div>
+                <div>{{ pComment.sendTime }}</div>
+                <div class="hover-pointer" style="margin-left: 60%"
+                     @click="replySubComment(pComment.commentId, '',true)">
+                  <i class="el-icon-chat-line-square"/>
+                  <span style="margin-left: 5px">回复</span>
+                </div>
+                <div class="hover-pointer" style="margin-left: 20px" @click="testMethod()">
+                  <i class="el-icon-thumb"/>
+                  <span style="margin-left: 5px">{{ pComment.likeCnt }}</span>
+                </div>
+              </div>
+              <div style="margin-bottom: 10px">
+                {{ pComment.content }}
+              </div>
+            </div>
+
+            <!--   每个母评论下的子评论列表       -->
+            <div v-for="subComment in pComment.subContents"
+                 style="width: 95%;margin-left: 5%;display: flex;flex-wrap: wrap">
+              <div style="margin-right: 10px">
+                <el-avatar :size="35" src="http://121.43.39.16:8082/ruoyi-test/2024/02/02/2b_20240202165949A006.jpg"/>
+              </div>
+
+              <div style="margin-bottom: 10px;display: flex;flex-wrap: wrap">
+                <div style="margin-bottom: 10px;display: flex;width: 100%">
+                  <div style="margin-right: 10px">{{ subComment.senderName }}</div>
+                  <div style="margin-right: 10px">回复</div>
+                  <div style="margin-right: 10px">{{ subComment.receiverName }}</div>
+                  <div style="margin-right: 10px">{{ subComment.sendTime }}</div>
+
+                  <div class="hover-pointer" @click="replySubComment(pComment.commentId, subComment.commentId, false)">
+                    <i class="el-icon-chat-line-square"/>
+                    <span style="margin-left: 5px">回复</span>
+                  </div>
+                  <div class="hover-pointer" style="margin-left: 20px" @click="testMethod()">
+                    <i class="el-icon-thumb"/>
+                    <span style="margin-left: 5px">{{ subComment.likeCnt }}</span>
+                  </div>
+
+                </div>
+                <div style="margin-bottom: 10px">
+                  {{ subComment.content }}
+                </div>
+              </div>
+            </div>
+
+            <!-- 回复评论的输入区域 -->
+            <div v-if="pComment.commentId === showCommentReplyId" style="width: 100%;display: flex;flex-wrap: wrap">
+              <div style="width: 70%">
+                <el-input
+                  v-model="commentReplyInput"
+                  :autosize="true"
+                  :placeholder="commentReplyHolder"
+                  :rows="2"
+                  :show-word-limit="true"
+                  maxlength="500"
+                  resize="none"
+                  type="textarea"
+                />
+              </div>
+
+              <div style="width: 30%;padding-left: 20%">
+                <el-button round type="primary" @click="testMethod()">回复</el-button>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+
+      </div>
+    </el-dialog>
 
 
   </div>
@@ -106,6 +216,7 @@ export default {
       }),
       testBlogs: [
         {
+          id: 1,
           username: 'name1',
           postTime: '2024年2月2日20:41:37',
           content: 'Tip: 使用 CSS 最大的好处是，如果把 CSS 代码存放到外部样式表中，那么站点会更易于维护。通过编辑单一的文件，就可以改变所有页面的布局。如需学习更多有关 CSS 的知识，请访问我们的CSS 教程。\n' +
@@ -128,42 +239,192 @@ export default {
             '\n' +
             'Tip: 由于创建高级的布局非常耗时，使用模板是一个快速的选项。通过搜索引擎可以找到很多免费的网站模板（您可以使用这些预先构建好的网站布局，并优化它们）。'
         }, {
+          id: 2,
           username: 'name2',
           postTime: '2024年2月2日20:41:37',
           content: 'hhhhhhhhhhh'
         }, {
+          id: 3,
           username: 'name3',
           postTime: '2024年2月2日20:41:37',
           content: 'hhhhhhhhhhh'
         }, {
+          id: 4,
           username: 'name4',
           postTime: '2024年2月2日20:41:37',
           content: 'hhhhhhhhhhh'
         }, {
+          id: 5,
           username: 'name5',
           postTime: '2024年2月2日20:41:37',
           content: 'hhhhhhhhhhh'
         }, {
+          id: 6,
           username: 'name6',
           postTime: '2024年2月2日20:41:37',
           content: 'hhhhhhhhhhh'
         }, {
+          id: 7,
           username: 'name7',
           postTime: '2024年2月2日20:41:37',
           content: 'hhhhhhhhhhh'
         }
-      ]
+      ],
+      testComment: {
+        cnt: 7,
+        content: [
+          {
+            commentId: 1,
+            parentId: -1,
+            senderId: '1',
+            senderName: '用户一',
+            receiverId: '-1',
+            receiverName: '',
+            sendTime: '2024年2月3日22:33:08',
+            likeCnt: 999,
+            content: '真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼真牛逼',
+            subContents: [
+              {
+                commentId: 2,
+                parentId: 1,
+                senderId: '2',
+                senderName: '用户二',
+                receiverId: '1',
+                receiverName: '用户一',
+                sendTime: '2024年2月3日22:33:08',
+                likeCnt: 999,
+                content: '真牛逼'
+
+              }, {
+                commentId: 3,
+                parentId: 1,
+                senderId: '3',
+                senderName: '用户三',
+                receiverId: '1',
+                receiverName: '用户一',
+                sendTime: '2024年2月3日22:33:08',
+                likeCnt: 999,
+                content: '真牛逼'
+              }
+            ]
+          }, {
+            commentId: 4,
+            parentId: -1,
+            senderId: '4',
+            senderName: '用户四',
+            receiverId: '-1',
+            receiverName: '',
+            sendTime: '2024年2月3日22:33:08',
+            likeCnt: 999,
+            content: '真牛逼',
+            subContents: [
+              {
+                commentId: 5,
+                parentId: 4,
+                senderId: '5',
+                senderName: '用户五',
+                receiverId: '4',
+                receiverName: '用户四',
+                sendTime: '2024年2月3日22:33:08',
+                likeCnt: 999,
+                content: '真牛逼'
+
+              }, {
+                commentId: 6,
+                parentId: 4,
+                senderId: '6',
+                senderName: '用户六',
+                receiverId: '4',
+                receiverName: '用户四',
+                sendTime: '2024年2月3日22:33:08',
+                likeCnt: 999,
+                content: '真牛逼'
+
+              }
+            ]
+          }, {
+            commentId: 7,
+            parentId: -1,
+            senderId: '7',
+            senderName: '用户七',
+            receiverId: '-1',
+            receiverName: '',
+            sendTime: '2024年2月3日22:33:08',
+            likeCnt: 999,
+            content: '真牛逼',
+            subContents: []
+          },
+        ]
+      },
+      showPostDetail: false,
+      showUser: {},
+      commentInput: '',
+      showCommentReplyId: '',
+      commentReplyHolder: '评论的回复:',
+      commentReplyInput: ''
     }
   },
   created() {
   },
-  methods: {}
+  methods: {
+    replySubComment(pCommentId, subCommentId, isParent) {
+      this.showCommentReplyId = pCommentId
+      let commentName = ''
+      let pComment = this.testComment.content.find(u => u.commentId === pCommentId);
+      if (isParent) {
+        commentName = pComment.senderName
+      } else {
+        commentName = pComment.subContents.find(u => u.commentId === subCommentId).senderName
+      }
+      this.commentReplyHolder = '回复 @' + commentName + ':'
+    },
+    testMethod() {
+      this.$message({
+        message: '获得响应！',
+        type: 'success'
+      })
+    },
+    showPost(userId) {
+      this.showUser = this.testBlogs.find(u => u.id === userId)
+      this.showPostDetail = true
+    },
+    handleClose() {
+      this.showPostDetail = false
+      this.commentInput = ''
+      this.showCommentReplyId = ''
+    }
+
+  }
 };
 
 
 </script>
 
 <style>
+.hover-pointer:hover {
+  cursor: pointer;
+}
+
+.comment-container {
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-bottom: 30px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.post-sub-title-unit {
+  margin-right: 30px;
+  float: left
+}
+
+.post-sub-title {
+  padding-top: 10px;
+  padding-bottom: 30px;
+  margin-bottom: 30px;
+  background-color: #EEF1F6FF
+}
+
 .fixed-box-left {
   background-color: rgb(238, 241, 246);
   padding-top: 30px;
@@ -189,7 +450,7 @@ export default {
 
 .card-button {
   float: left;
-  width: 33%;
+  width: 50%;
   text-align: center;
   margin-top: 30px;
   margin-bottom: 20px;
